0e7150678efc05bded8e32f1bedf44d74e94cb90
asmaaramadan3333 <asmaaramadan3333@github.com>
2021-01-29 15:12:58 +0200

    first commit

---
+++ pharmacy-mnagement-system/src/main/java/graduation/demo/pharmacymanagmentsystem/PharmacySystemApplication.java
@@ -0,0 +1,13 @@
+package graduation.demo.pharmacymanagmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacySystemApplication.class, args);
+	}
+
+}
