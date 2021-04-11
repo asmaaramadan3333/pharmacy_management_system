bcc7026916489b708fb5f1a1a53afaa8b1980c5f
asmaaramadan3333 <asmaa.ramadan3333@gmail.com>
2021-04-11 23:25:29 +0200

    edit test

--- pharmacy-mnagement-system/src/test/java/graduation/project/pharmacysystem/PharmacySystemApplicationTests.java
+++ pharmacy-mnagement-system/src/test/java/graduation/project/pharmacysystem/PharmacySystemApplicationTests.java
@@ -2,13 +2,10 @@ package graduation.project.pharmacysystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-<<<<<<< HEAD
-=======
 import graduation.demo.pharmacymanagementsystem.PharmacySystemApplication;
 
 
->>>>>>> branch 'main' of https://github.com/asmaaramadan3333/pharmacy_management_system.git
-import graduation.demo.pharmacymanagementsystem.PharmacySystemApplication;
+
 
 @SpringBootTest(classes = PharmacySystemApplication.class)
 class PharmacySystemApplicationTests {
