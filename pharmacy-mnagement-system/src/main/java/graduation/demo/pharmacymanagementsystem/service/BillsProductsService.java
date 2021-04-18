9854d197b56dba68ba50259f1550f33c395c4ebc
asmaaramadan3333 <74137653+asmaaramadan3333@users.noreply.github.com>
2021-04-18 03:46:16 +0200

    Update BillsProductsService.java

--- pharmacy-mnagement-system/src/main/java/graduation/demo/pharmacymanagementsystem/service/BillsProductsService.java
+++ pharmacy-mnagement-system/src/main/java/graduation/demo/pharmacymanagementsystem/service/BillsProductsService.java
@@ -2,12 +2,16 @@ package graduation.demo.pharmacymanagementsystem.service;
 
 import java.util.List;
 import java.util.Map;
-
+import graduation.demo.pharmacymanagementsystem.entity.Bill;
 import graduation.demo.pharmacymanagementsystem.dto.BillsProductDTO;
 import graduation.demo.pharmacymanagementsystem.entity.BillsProduct;
 
 public interface BillsProductsService {
 
 	public List<BillsProductDTO> find_BillsProductby_Bill_ID(long bill_id);
+
+	public void saveORupdate (List<BillsProduct> theBillsProducts);
+	public List<BillsProduct> findAllBillsProducts();
+
 
 }
